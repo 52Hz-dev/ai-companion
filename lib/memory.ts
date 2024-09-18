79c996051db1eb0c80d1f282d30c3ba6bfdc1d5e
screepy <huutai1515225@gmail.com>
2024-09-18 20:42:50 +0700

    config PineconeDB

--- lib/memory.ts
+++ lib/memory.ts
@@ -1,7 +1,7 @@
 import {Redis} from "@upstash/redis";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
-import { PineconeStore } from "@langchain/pinecone";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 export type CompanionKey={
     companionName:string;
@@ -16,24 +16,26 @@ export class MemoryManager{
 
     public constructor(){
         this.history=Redis.fromEnv();
-        this.vectorDBClient=new PineconeClient(
-            {
-                apiKey:process.env.PINECONE_API_KEY!
-            }
-        );
+        this.vectorDBClient=new PineconeClient();
     }
     public async init(){
         if(this.vectorDBClient instanceof PineconeClient){
-            
+            if(this.vectorDBClient instanceof PineconeClient){
+                await this.vectorDBClient.init({
+                    apiKey:process.env.PINECONE_API_KEY!,
+                    environment:process.env.PINECONE_API_KEY!,
+                });
+            }
         }
     }
     public async vectorSearch(
-        recentChatHistory:number[],
+        recentChatHistory:string,
         companionFileName:string
     ){
         const pineconeClient=<PineconeClient>this.vectorDBClient;
 
-        const pineconeIndex=pineconeClient.Index(process.env.PINECONE_INDEX!||"");
+        const pineconeIndex=pineconeClient.Index(
+            process.env.PINECONE_INDEX!||"");
 
         const vectorStore=await PineconeStore.fromExistingIndex(
             new OpenAIEmbeddings({openAIApiKey:process.env.OPEN_API_KEY}),
@@ -43,16 +45,73 @@ export class MemoryManager{
 
 
         const similarDocs=await vectorStore
-        .similaritySearchVectorWithScore(recentChatHistory,2)
+        .similaritySearch(recentChatHistory,3,{fileName:companionFileName})
         .catch((err)=>{
             console.log("Failed to get vector search results",err);
 
         })
         return similarDocs;
     }
-    // public static async getInstance():Promise<MemoryManager>{
-    //     if(!MemoryManager.instance){
+    public static async getInstance():Promise<MemoryManager>{
+        if(!MemoryManager.instance){
+            MemoryManager.instance=new MemoryManager();
+            await MemoryManager.instance.init();
+        }
+        return MemoryManager.instance;
+    }
 
-    //     }
+    private generateRedisCompanionKey(CompanionKey: CompanionKey):string{
+        return `${CompanionKey.companionName}-${CompanionKey.modelName}-${CompanionKey.userId}`;
+    }
+
+    public async writeToHistory(text:string,companionKey:CompanionKey){
+        if(!companionKey|| typeof companionKey.userId=='undefined'){
+            console.log("Companion key set incorrectly");
+            return "";
+        }
+
+        const key=this.generateRedisCompanionKey(companionKey);
+        const result=await this.history.zadd(key,{
+            score:Date.now(),
+            member:text,
+
+        })
+        return result;
+    }
+    public async readLatedHistory(companionKey:CompanionKey):Promise<string>{
+        if(!companionKey|| typeof companionKey.userId=="undefined"){
+                console.log("Companion key set incorrectly");
+                return '';
+        }
+        const key=this.generateRedisCompanionKey(companionKey);
+        let result=await this.history.zrange(key,0,Date.now(),{
+byScore:true
+        })
+        result =result.slice(-30).reverse();
+        const recentChats=result.reverse().join("\n");
+        return recentChats;
+    }
+
+    public async seedChatHistory(
+        seedContent:String,
+        delimiter: string="\n",
+        companionKey:CompanionKey,
+    ){
+        const key=this.generateRedisCompanionKey(companionKey);
+
+        if(await this.history.exists(key)){
+            console.log("User already has chat history");
+            return
+        }
+
+        const content =seedContent.split(delimiter);
+        let counter=0;
+        for (const line of content){
+            await this.history.zadd(key,{
+                score :counter,
+                member:line
+            });
+            counter+=1;
+        }
     }
 }
